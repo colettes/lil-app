36976ee71214e976d738279c1818bd09550eb2ed
colettes <colette11295@gmail.com>
2021-03-27 13:17:56 -0500

    Initial commit

---
+++ src/Pages/Main.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className='Main'>
+                <h1 className='Main-header'>Main Page</h1>
+            </div>
+        )
+    }
+}
+
+export default Main;
