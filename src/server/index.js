a2f545d6b0e5ab3c0eebd087badff938084a1c6c
Colettes <colette11295@gmail.com>
2021-04-04 20:53:29 +0100

    Add sqlite database

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('db/app.db')
+
 const port = 3001;
 
 app.use(function(req, res, next) {
@@ -13,7 +17,14 @@ app.get('/', (req, res) => {
 });
 
 app.get('/items', (req, res) => {
-    res.json({items: ['a', 'z', 'c']});
+    const sql = 'SELECT * FROM items';
+    db.all(sql, (error, rows) => {
+        if (error) throw error;
+        const result = {items: rows};
+        res.json(result);
+    });
+    
+    
 });
 
 app.listen(port, () => {
