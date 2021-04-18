e339f36581cb16358d4f2da133c912248ff7e061
Colette Smith <colette@Colettes-MacBook-Air.local>
2021-04-18 12:08:24 -0500

    Update .gitignore

---
+++ db/sql-commands.sql
@@ -0,0 +1,35 @@
+CREATE TABLE items (
+    id TEXT PRIMARY KEY,
+    image_url TEXT,
+    title TEXT,
+    description TEXT
+);
+
+INSERT INTO items VALUES(
+    'ba00e8a4-90a6-4ff0-850d-90d1e224402f', 
+    'https://images.squarespace-cdn.com/content/v1/5ebad749d483617950baa507/1604671323607-4DO2MA8M1L5XN4TWFIOA/ke17ZwdGBToddI8pDm48kLIDLBp5z4LZxwf0D5pzcXp7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmHHRMqniMJbuwH8EZRFFu5caYSaGKInkGFgQ52gStTVsfqlcOgesUaAnSJBL_2MmH/Summer.2020.007.jpg?format=750w',
+    'Summer 2020 007',
+    'Print of mixed media painting, Summer 2020 007. Printed on heavyweight 100% cotton hot press paper with archival pigment inks.  Each print is signed on the back so you can orient the piece however you like. '
+    );
+
+INSERT INTO items VALUES(
+    '11a336f6-d875-442e-8220-b08dd976f793', 
+    'https://images.squarespace-cdn.com/content/v1/5ebad749d483617950baa507/1589380633767-WDJJNNSWYGLL7W5XYSY9/ke17ZwdGBToddI8pDm48kHWJhM5OumnWkn-bJp56T597gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UcL9jD0a5X9v0pRv3bkEBlbMdRGmLpxxWT_kpX2tG1kWONPeA8knsC0EEyRnv-0ugw/004.jpg?format=750w',
+    '2019.032.Untitled Print',
+    'Print of mixed media painting, 2019.032.Untitled Print. Printed on heavyweight 100% cotton hot press paper with archival pigment inks.  Each print is signed on the back so you can orient the piece however you like. '
+    );
+
+INSERT INTO items VALUES(
+    '92e1fa73-d830-4842-8942-cf192fa05a4b',
+    'https://images.squarespace-cdn.com/content/v1/5ebad749d483617950baa507/1604670974260-EMOVJYJF9CTZ7E6HIT2W/ke17ZwdGBToddI8pDm48kLIDLBp5z4LZxwf0D5pzcXp7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmHHRMqniMJbuwH8EZRFFu5caYSaGKInkGFgQ52gStTVsfqlcOgesUaAnSJBL_2MmH/Summer.2020.001.small.jpg?format=500w',
+    'Summer.2020.001 Print',
+    'Print of mixed media painting, Summer.2020.001. Printed on heavyweight 100% cotton hot press paper with archival pigment inks.  Each print is signed on the back so you can orient the piece however you like. '
+    );
+
+INSERT INTO items VALUES(
+    '4552794f-db53-40be-a39d-6cc09cd37fe9',
+    'https://images.squarespace-cdn.com/content/v1/5ebad749d483617950baa507/1589381917746-MGDS33I26LWV9T6IN8WD/ke17ZwdGBToddI8pDm48kH-NTu5of1wUeVVfTOIw5Ll7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1US8vG9N9LUAOa-7oqQJx3Aj4qdCZLDdCQGjAVkuqI_H3tP5em5xMxOQsKJAmH2x2lQ/2019.028.Untitled.jpg?format=500w',
+    '2019.028.Untitled Print',
+    'Print of mixed media painting, 2019.028.Untitled. Printed on heavyweight 100% cotton hot press paper with archival pigment inks.  Each print is signed on the back so you can orient the piece however you like. '
+    );
+
--- src/TODO.md
+++ src/TODO.md
@@ -1,8 +1,19 @@
 Navigation
-Detail View Pages
+Detail view Pages
 
 Features:
     favorite art
     follow artists
     admin screen
+    artist profile
+    user profile
 
+
+
+
+Use cases:
+    user creates profile
+    user adds favorites/saves art
+    user can follow specific artists
+    
+    admin 
