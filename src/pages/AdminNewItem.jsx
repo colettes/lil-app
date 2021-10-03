c307e6d5dc2e5bb2306ee13543e02bde9f29cde5
Colette Smith <colette11295@gmail.com>
2021-10-03 15:36:34 -0500

    Reimplement AdminNewItem as hook component

--- src/pages/AdminNewItem.jsx
+++ src/pages/AdminNewItem.jsx
@@ -1,60 +1,88 @@
-import React, { Component } from 'react';
+import React, { Component, useReducer } from 'react';
+import { useHistory } from "react-router-dom";
+import { cloneDeep, trim } from 'lodash';
 
-class AdminNewItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            form: {
-                title: '',
-                description: '',
-                url: '',
-                artist: '',
-            }
-        };
-    }
-
-    createItem(e) {
-        e.preventDefault();
-        const options = {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(this.state.form)
-        };
-        fetch('/items', options)
-            .then(() => this.props.history.push('/admin'));
-    }
-
-    updateFormState(field, e) {
-        const newForm = Object.assign({}, this.state.form, { [field]: e.target.value });
-        this.setState({ form: newForm });
-    }
-
-    render() {
-        const { form } = this.state;
-        return (
-            <div className="AdminNewItem Page">
-                <form onSubmit={(e) => this.createItem(e)}>
-                    <div>
-                        <input type="text" value={form.title} placeholder="Title" onChange={(e) => this.updateFormState('title', e)} />
-                    </div>
-                    <div>
-                        <input type="text" value={form.description} placeholder="Description" onChange={(e) => this.updateFormState('description', e)} />
-                    </div>
-                    <div>
-                        <input type="text" value={form.url} placeholder="Image URL" onChange={(e) => this.updateFormState('url', e)} />
-                    </div>
-                    <div>
-                        <input type="text" value={form.artist} placeholder="Artist" onChange={(e) => this.updateFormState('artist', e)} />
-                    </div> 
-                    <div>
-                        <input type="submit" value="Create" />
-                    </div>
-                </form>
-            </div>
-        );
-    }
+function PureAdminNewItem(props) {
+    return (
+        <div className="AdminNewItem Page">
+            <form onSubmit={(e) => props.createItem(e)}>
+                <div>
+                    <input type="text" value={props.state.form.title} placeholder="Title" onChange={(e) => props.updateFormState('title', e)} />
+                </div>
+                <div>
+                    <input type="text" value={props.state.form.description} placeholder="Description" onChange={(e) => props.updateFormState('description', e)} />
+                </div>
+                <div>
+                    <input type="text" value={props.state.form.url} placeholder="Image URL" onChange={(e) => props.updateFormState('url', e)} />
+                </div>
+                <div>
+                    <input type="text" value={props.state.form.artist} placeholder="Artist" onChange={(e) => props.updateFormState('artist', e)} />
+                </div> 
+                <div>
+                    <input type="submit" value="Create" />
+                </div>
+            </form>
+        </div>
+    );
 }
+
+const initialState = {
+    form: {
+        title: '',
+        description: '', 
+        url: '',
+        artist: '',
+    }
+};
+
+function reducer(oldState, action) {
+    switch(action.type) {
+        case 'updateFormState':
+            const newState = cloneDeep(oldState);
+            newState.form[action.fieldName] = trim(action.fieldValue);
+            console.log(action);
+            return(newState);
+        default:
+            throw new Error('Unknown action type!');
+    }
+};
+function useAdminNewItem(){
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const history = useHistory();
+    const updateFormState = (fieldName, e) => dispatch({type:'updateFormState', fieldName:fieldName, fieldValue:e.target.value});
+    
+    return(
+        {
+            state, 
+            updateFormState, 
+            createItem: (e) => createItem(history, state, e)
+        }
+    );
+}
+function AdminNewItem() {
+    const {state, updateFormState, createItem} = useAdminNewItem();
+
+    return(
+        <PureAdminNewItem 
+            state={state} 
+            createItem={createItem} 
+            updateFormState={updateFormState}
+        />
+    );
+}
+
+function createItem(history, state, e) {
+    e.preventDefault();
+    const options = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(state.form)
+    };
+    fetch('/items', options)
+        .then(() => history.push('/admin'));
+}
+
 export default AdminNewItem;
