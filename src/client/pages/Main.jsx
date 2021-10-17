b422bc0966b82c8a3e784b0c51fcb78b38a2ec2a
Colette Smith <colette11295@gmail.com>
2021-10-17 14:14:59 -0500

    Reimplement Main as hook component

--- src/client/pages/Main.jsx
+++ src/client/pages/Main.jsx
@@ -8,7 +8,7 @@ function PureMain(props) {
   return (
     <div className="Main Page">
       <h1>Lil App</h1>
-      <Login loadData={(user) => props.loadData(user)} />
+      <Login loadData={() => props.loadData()} />
       <ol>
         {!items && <li>loading</li>}
         {items &&
@@ -22,33 +22,26 @@ function PureMain(props) {
               <div>
                 <FavoriteButton
                   onClick={() => props.favoriteItem(item.id, item.favorited)}
-                  favorited={item.favorited}
-                />
+                  favorited={item.favorited}/>
               </div>
             </li>
           ))}
       </ol>
-      <pre>{JSON.stringify(this.state, null, 2)}</pre>
+      <pre>{JSON.stringify(props.state, null, 2)}</pre>
     </div>
   );
 }
 
-const FavoriteButton = (props) => {
-  const { onClick, favorited } = props;
-  const text = favorited ? "Unfavorite" : "Favorite";
-  return <button onClick={onClick}>{text}</button>;
-};
-
-function loadData2(user, dispatch) {
+function loadData(dispatch) {
   fetch("/me")
     .then((res) => res.json())
-    .then((json, user) => dispatch({ type: "receiveData", user: json }));
+    .then((json) => dispatch({ type: 'receiveUser', user: json}));
   fetch("http://localhost:3000/items")
     .then((res) => res.json())
-    .then((json) => dispatch({ type: "receiveData", json }));
+    .then((json) => dispatch({ type: 'receiveItems',  items: json.items}));
 }
 
-function favoriteItem2(itemID, favorited, dispatch) {
+function favoriteItem(itemID, favorited, dispatch) {
   const options = {
     method: favorited ? "DELETE" : "POST",
     headers: {
@@ -57,86 +50,30 @@ function favoriteItem2(itemID, favorited, dispatch) {
     },
     body: JSON.stringify({ itemID }),
   };
-  fetch("/favorites", options).then((user) => loadData(user, dispatch));
+  fetch("/favorites", options).then(() => loadData(dispatch));
 }
 
-function Main2(props) {
+function Main() {
   const [state, dispatch] = useReducer(reducer, {});
-  useEffect((user) => {loadData(user, dispatch);}, []);
+  useEffect(() => {loadData(dispatch)}, []);
 
-  return(
-      <PureMain
-        state={state}
-        loadData={(user) => loadData(user, dispatch)}
-        favoriteItem={(itemID, favorited) => favoriteItem(itemID, favorited)}/>
-  );
+  return (
+    <PureMain
+      state={state}
+      loadData={() => loadData(dispatch)}
+      favoriteItem={(itemID, itemFavorited) => favoriteItem(itemID, itemFavorited, dispatch)} />
+  )
 }
 
 function reducer(oldState, action) {
-  console.log(action);
-}
-
-class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  componentDidMount() {
-    this.loadData();
-  }
-
-  loadData(user) {
-    fetch("/me")
-      .then((res) => res.json())
-      .then((json) => this.setState({ user: json }));
-    fetch("http://localhost:3000/items")
-      .then((res) => res.json())
-      .then((json) => this.setState(json));
-  }
-
-  favoriteItem(itemID, favorited) {
-    const options = {
-      method: favorited ? "DELETE" : "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ itemID }),
-    };
-    fetch("/favorites", options).then(() => this.loadData());
-  }
-
-  render() {
-    const { items } = this.state;
-
-    return (
-      <div className="Main Page">
-        <h1>Lil App</h1>
-        <Login loadData={(user) => this.loadData(user)} />
-        <ol>
-          {!items && <li>loading</li>}
-          {items &&
-            items.map((item) => (
-              <li key={item.id}>
-                {item.title}
-                {item.artist}
-                <div>
-                  <img src={item.image_url} alt={item.description} />
-                </div>
-                <div>
-                  <FavoriteButton
-                    onClick={() => this.favoriteItem(item.id, item.favorited)}
-                    favorited={item.favorited}
-                  />
-                </div>
-              </li>
-            ))}
-        </ol>
-        <pre>{JSON.stringify(this.state, null, 2)}</pre>
-      </div>
-    );
-  }
+  switch(action.type) {
+    case 'receiveUser':
+      return(Object.assign({}, oldState, {user: action.user}));
+    case 'receiveItems':
+      return(Object.assign({}, oldState, {items: action.items}));
+    default:
+      throw new Error();
+  };
 }
 
 const FavoriteButton = (props) => {
