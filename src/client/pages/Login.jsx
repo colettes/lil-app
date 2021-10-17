d5c461f4c7027f4211d75a4e37c1cd353d26216c
Colette Smith <colette11295@gmail.com>
2021-10-17 14:28:57 -0500

    Convert Login to function component

--- src/client/pages/Login.jsx
+++ src/client/pages/Login.jsx
@@ -1,24 +1,20 @@
 import React, {Component} from 'react';
 
-class Login extends Component {
-    login() {
-        const options = {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({username: 'Colette', password: 'password'})
-        };
-        fetch('/login', options)
-            .then(() => this.props.loadData());
+function login(loadData) {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({username: 'Colette', password: 'password'})
     };
+    fetch('/login', options)
+        .then(() => loadData());
+};
 
-    render() {
-        return(
-            <button onClick={() => this.login()}>login</button>
-        );
-    }
+function Login(props) {
+    return <button onClick={() => login(props.loadData)}>login</button>;
 }
 
 export default Login;
